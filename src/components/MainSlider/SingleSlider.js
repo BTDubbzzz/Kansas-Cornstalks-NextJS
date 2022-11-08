17ecdd05b52bacefadeb225e4f372c4360c61b65
Blake <btdubbzzz@gmail.com>
2022-11-07 23:12:45 -0600

    Production version 1 of Intelligent Landing Page

--- src/components/MainSlider/SingleSlider.js
+++ src/components/MainSlider/SingleSlider.js
@@ -1,47 +1,44 @@
-import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import Link from "../Reuseable/Link";
-import TextSplit from "../Reuseable/TextSplit";
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import Link from '../Reuseable/Link';
+import TextSplit from '../Reuseable/TextSplit';
+import bg1 from '@/images/backgrounds/lawn-hi-res.jpeg';
+import WorkOne from '../Work/WorkOne';
 
 const SingleSlider = ({ slide = {}, sliderTwo = false }) => {
-  const { bg, text, title } = slide;
+	const { bg, text, title } = slide;
 
-  return (
-    <>
-      <div
-        className="image-layer"
-        style={{
-          backgroundImage: `url(${
-            require(`@/images/backgrounds/${bg}`).default.src
-          })`,
-        }}
-      ></div>
-      <Container>
-        <Row>
-          <Col xl={12}>
-            <div
-              className={`main-slider-one__inner${
-                !sliderTwo ? " text-center" : ""
-              }`}
-            >
-              {sliderTwo && <div className="border-box"></div>}
-              <p className="main-slider-one__text">{text}</p>
-              <TextSplit
-                text={title}
-                as="h2"
-                className="main-slider-one__title"
-              />
-              <div className="main-slider-one__btn">
-                <Link href="#" className="thm-btn">
-                  {sliderTwo ? "Find Product" : "Our Solution"}
-                </Link>
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<div
+				className="image-layer"
+				style={{
+					backgroundImage: `url(${bg1.src})`,
+				}}
+			></div>
+			<Container>
+				<Row>
+					<Col xl={12}>
+						<div
+							className={`main-slider-one__inner${!sliderTwo ? ' text-center' : ''}`}
+						>
+							{sliderTwo && <div className="border-box"></div>}
+							<p className="main-slider-one__text">{text}</p>
+							<TextSplit text={title} as="h2" className="main-slider-one__title" />
+							<div>
+								<WorkOne></WorkOne>
+							</div>
+							{/* <div className="main-slider-one__btn">
+								<Link href="/projects" className="thm-btn">
+									{sliderTwo ? 'Find Product' : 'See our work'}
+								</Link>
+							</div> */}
+						</div>
+					</Col>
+				</Row>
+			</Container>
+		</>
+	);
 };
 
 export default SingleSlider;
--- src/data/mainSlider.js
+++ src/data/mainSlider.js
@@ -1,47 +1,47 @@
 export const mainSlider = [
-  {
-    id: 1,
-    bg: "main-slider-v1-1.jpg",
-    text: "A Complete Solution for Your Flooring Vision.",
-    title: "Welcome to Our Alori \n Flooring Solutions",
-  },
-  {
-    id: 2,
-    bg: "main-slider-v1-2.jpg",
-    text: "A Complete Solution for Your Flooring Vision.",
-    title: "Welcome to Our Alori \n Flooring Solutions",
-  },
-  {
-    id: 3,
-    bg: "main-slider-v1-3.jpg",
-    text: "A Complete Solution for Your Flooring Vision.",
-    title: "Welcome to Our Alori \n Flooring Solutions",
-  },
-  {
-    id: 4,
-    bg: "main-slider-v1-4.jpg",
-    text: "A Complete Solution for Your Flooring Vision.",
-    title: "Welcome to Our Alori \n Flooring Solutions",
-  },
+	{
+		id: 1,
+		bg: 'lawn-hi-res.jpeg',
+		text: 'Full site coming soon',
+		title: 'Welcome to Intelligent Lawncare',
+	},
+	// {
+	// 	id: 2,
+	// 	bg: 'main-slider-v1-2.jpg',
+	// 	text: 'test text 2',
+	// 	title: 'Welcome to Our Alori \n Flooring Solutions',
+	// },
+	// {
+	// 	id: 3,
+	// 	bg: 'main-slider-v1-3.jpg',
+	// 	text: 'test text 3',
+	// 	title: 'Welcome to Our Alori \n Flooring Solutions',
+	// },
+	// {
+	// 	id: 4,
+	// 	bg: 'main-slider-v1-4.jpg',
+	// 	text: 'test text 4.',
+	// 	title: 'Welcome to Our Alori \n Flooring Solutions',
+	// },
 ];
 
 export const mainSliderTwo = [
-  {
-    id: 1,
-    bg: "main-slider-v2-1.jpg",
-    text: "A Complete Solution for Your Flooring Vision.",
-    title: "Your Floor, Your \nLifestyle, Our Passion",
-  },
-  {
-    id: 2,
-    bg: "main-slider-v1-2.jpg",
-    text: "A Complete Solution for Your Flooring Vision.",
-    title: "Your Floor, Your \nLifestyle, Our Passion",
-  },
-  {
-    id: 3,
-    bg: "main-slider-v1-3.jpg",
-    text: "A Complete Solution for Your Flooring Vision.",
-    title: "Your Floor, Your \nLifestyle, Our Passion",
-  },
+	{
+		id: 1,
+		bg: 'main-slider-v2-1.jpg',
+		text: 'A Complete Solution for Your Flooring Vision.',
+		title: 'Your Floor, Your \nLifestyle, Our Passion',
+	},
+	{
+		id: 2,
+		bg: 'main-slider-v1-2.jpg',
+		text: 'A Complete Solution for Your Flooring Vision.',
+		title: 'Your Floor, Your \nLifestyle, Our Passion',
+	},
+	{
+		id: 3,
+		bg: 'main-slider-v1-3.jpg',
+		text: 'A Complete Solution for Your Flooring Vision.',
+		title: 'Your Floor, Your \nLifestyle, Our Passion',
+	},
 ];
