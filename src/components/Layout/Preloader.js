17ef0d59e921cdc8f8a3bbcea672c1a8e5fdc16b
Blake <btdubbzzz@gmail.com>
2022-11-08 01:00:56 -0600

    changed loaded to intelligent logo

--- src/components/Layout/Preloader.js
+++ src/components/Layout/Preloader.js
@@ -1,18 +1,18 @@
-import image from "@/images/loader.png";
-import React from "react";
-import { Image } from "react-bootstrap";
+import image from '@/images/intelligent-logo-transparent-small.png';
+import React from 'react';
+import { Image } from 'react-bootstrap';
 
 const Preloader = ({ loading = true }) => {
-  return (
-    <div
-      style={{
-        zIndex: loading ? 9999 : -1,
-      }}
-      className={`preloader animated${loading ? "" : " fadeOut"}`}
-    >
-      <Image className="preloader__image" width={60} src={image.src} alt="" />
-    </div>
-  );
+	return (
+		<div
+			style={{
+				zIndex: loading ? 9999 : -1,
+			}}
+			className={`preloader animated${loading ? '' : ' fadeOut'}`}
+		>
+			<Image className="preloader__image" width={60} src={image.src} alt="" />
+		</div>
+	);
 };
 
 export default Preloader;
